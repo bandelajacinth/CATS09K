abcac9921ff85e3599cf08dd663009b405a6950a
bandelajacinth <117167463+bandelajacinth@users.noreply.github.com>
2024-01-30 15:50:45 +0530

    Add files via upload

---
+++ DIGITS_COUNT.cpp
@@ -0,0 +1,28 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+int n=1234;
+int rem=0;
+int count=0;
+while(n>0)
+{
+rem=n%10;
+count++;
+n=n/10;
+}
+cout<<count;
+}*/
+#include<iostream>
+using namespace std;
+int main()
+{
+int n=1234;
+int count=0;
+while(n>0)
+{
+count++;
+n=n/10;
+}
+cout<<count;
+}
